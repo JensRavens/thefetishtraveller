c4f9214d072cd9755c1cc1c23ada6c248b18a6ef
Jens Ravens <jens@nerdgeschoss.de>
2018-04-26 02:43:10 +0200

    add url file upload helper to models

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,9 @@
+require "open-uri"
+
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def attach_from_url(attribute, url)
+    public_send(attribute).attach(io: open(url), filename: "hero.jpg", content_type: "image/jpg")
+  end
 end
