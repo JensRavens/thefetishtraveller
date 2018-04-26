0e8bb0292843316065b33c238216f47b75a0f535
Jens Ravens <jens@nerdgeschoss.de>
2018-04-26 03:25:23 +0200

    don’t show old events on home

--- app/javascript/application/pages/event_search.tsx
+++ app/javascript/application/pages/event_search.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import {connect} from 'react-redux';
 
 import {DB, State} from '../state';
-import {EventWithLocation, joinLocation} from '../models/event';
+import {EventWithLocation, joinLocation, chronological} from '../models/event';
 import {Like, isLiked} from '../models/like';
 import {EventListing} from '../components/event_listing';
 import Container from '../components/container';
@@ -16,7 +16,7 @@ interface Props {
 
 const mapStateToProps: (state: State) => Props = (state) => {
   return {
-    events: joinLocation(new DB(state).table('events').all.sort((a,b) => (a.startAt as any) - (b.startAt as any)), state),
+    events: joinLocation(new DB(state).table('events').all.sort(chronological), state),
     likes: new DB(state).table('likes').all
   }
 }
