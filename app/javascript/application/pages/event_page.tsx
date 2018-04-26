c3689c5c9ee8ef7951d3c8d18ed2cdcc5b84c617
Jens Ravens <jens@nerdgeschoss.de>
2018-04-26 04:19:30 +0200

    add related events

--- app/javascript/application/pages/event_page.tsx
+++ app/javascript/application/pages/event_page.tsx
@@ -9,6 +9,8 @@ import {APISession} from '../api';
 import Container from '../components/container';
 import Hero from '../components/hero';
 import LikeButton from '../components/like-button';
+import Listing from '../components/listing';
+import {EventListing} from '../components/event_listing';
 import {dateRange} from '../util';
 
 interface Props {
@@ -17,6 +19,7 @@ interface Props {
   like?: Like;
   editable: boolean;
   dispatch: (DBAction) => void;
+  otherEvents: EventWithLocation[];
 }
 
 const likes = writeDB.table('likes');
@@ -31,7 +34,7 @@ class EventPage extends React.Component<Props> {
   }
 
   render() {
-    let {event, like, editable} = this.props;
+    let {event, like, editable, otherEvents} = this.props;
     if(!event) { return null };
     const backgroundImage = event.hero && event.hero.big;
     return (
@@ -47,6 +50,13 @@ class EventPage extends React.Component<Props> {
           <div className="text-center"><LikeButton active={!!like} onClick={() => !!like ? this.unlike(like) : this.like()}/></div>
           {editable && <div><Link to={`/events/${event.id}/edit`}>edit</Link></div>}
         </Container>
+        {otherEvents.length && (
+          <React.Fragment>
+            <div className="spacer"/>
+            <h2>Other Events in {event.location.name}</h2>
+            <Listing>{otherEvents.map(e => <EventListing event={e} />)}</Listing>
+          </React.Fragment>
+        )}
       </React.Fragment>
     )
   }
@@ -56,9 +66,10 @@ const mapStateToProps = (state: State, props) => {
   const id: string = props.match.params.id;
   const db = new DB(state);
   let event: EventWithLocation | null = joinLocation([id], state)[0];
+  let otherEvents: EventWithLocation[] = joinLocation(db.table('events').where({locationId: event.locationId}).filter(e => e.id != id), state);
   const editable = canEdit(event, db.get('session'));
   const like = db.table('likes').where({eventId: id})[0]
-  return {event, id, like, editable};
+  return {event, id, like, editable, otherEvents};
 }
 
 export default connect(mapStateToProps)(EventPage)
