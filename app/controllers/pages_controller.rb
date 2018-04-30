a46cb6164bfb8fd9faad3805d14d7f0e3feb1d0e
Jens Ravens <jens@nerdgeschoss.de>
2018-04-30 20:07:06 +0200

    add meta and open graph tags

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
   def show
+    segments = params[:path].to_s.split('/')
+    @event = Event.published.find_by(slug: segments.second) if segments.size > 1 && segments.first == 'events'
   end
 end
