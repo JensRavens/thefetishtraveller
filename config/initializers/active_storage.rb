4c7a99174773fe54e071197272a88eeada040313
Jens Ravens <jens@nerdgeschoss.de>
2019-02-17 13:54:37 +0100

    allow caching assets from active storage

---
+++ config/initializers/active_storage.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module ActiveStorage
+  class RepresentationsController < BaseController
+    include ActiveStorage::SetBlob
+
+    def show
+      expires_in 1.year, public: true
+      variant = @blob.representation(params[:variation_key]).processed
+      send_data @blob.service.download(variant.key),
+        type: @blob.content_type || DEFAULT_SEND_FILE_TYPE,
+        disposition: 'inline'
+    end
+  end
+end
