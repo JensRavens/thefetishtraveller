dc8fa8f3b1a8acfd9a6f3b50926f7dff83ed2337
Jens Ravens <jens@nerdgeschoss.de>
2019-02-17 14:25:52 +0100

    fix monkey patching

--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+# make sure it is loaded
+ActiveStorage::RepresentationsController
+
 module ActiveStorage
   class RepresentationsController < BaseController
     include ActiveStorage::SetBlob
