705cb44119a7acb9a883a1b3c9fe1c01ba7dfe26
Jens Ravens <jens@nerdgeschoss.de>
2020-01-17 21:20:02 +0100

    env host for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,4 +89,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   Rails.application.routes.default_url_options[:protocol] = "https"
+  Rails.application.routes.default_url_options[:host] = ENV["HOST"]
 end
