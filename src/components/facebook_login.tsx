7b9388e55551d8d0ab0a25d013fff9aa59689271
Jens Ravens <jens@nerdgeschoss.de>
2020-01-17 21:35:35 +0100

    fix login

--- src/components/facebook_login.tsx
+++ src/components/facebook_login.tsx
@@ -1,30 +1,7 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useEffect } from 'react';
 
-import { APISession } from '../api';
-import { State } from '../state';
-
-interface Props {
-  session?: APISession;
-}
-
-class FacebookLogin extends React.Component<Props> {
-  public render() {
-    return (
-      <div
-        className="fb-login-button"
-        data-max-rows="1"
-        data-size="medium"
-        data-button-type="continue_with"
-        data-show-faces="false"
-        data-auto-logout-link="true"
-        data-use-continue-as="true"
-        data-scope="public_profile,email"
-      />
-    );
-  }
-
-  public componentDidMount() {
+export default function FacebookLogin(): JSX.Element {
+  useEffect(() => {
     const id = 'facebook-jssdk';
     const fjs = document.getElementsByTagName('script')[0];
     if (document.getElementById(id)) {
@@ -34,11 +11,17 @@ class FacebookLogin extends React.Component<Props> {
     js.id = id;
     js.src = 'https://connect.facebook.net/en_US/sdk.js';
     fjs.parentNode!!.insertBefore(js, fjs);
-  }
+  }, []);
+  return (
+    <div
+      className="fb-login-button"
+      data-max-rows="1"
+      data-size="medium"
+      data-button-type="continue_with"
+      data-show-faces="false"
+      data-auto-logout-link="true"
+      data-use-continue-as="true"
+      data-scope="public_profile,email"
+    />
+  );
 }
-
-const mapStateToProps: (state: State) => Props = state => {
-  return { session: state.settings.session };
-};
-
-export default connect(mapStateToProps)(FacebookLogin);
