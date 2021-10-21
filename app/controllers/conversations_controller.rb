165137289517475bdb9b93f47059125b61c69380
Jens Ravens <jens@nerdgeschoss.de>
2021-10-21 18:11:47 +0200

    bigger conversations page size

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -4,6 +4,6 @@ class ConversationsController < ApplicationController
   before_action :require_login
 
   def index
-    @conversations = paginated current_user.conversations.with_messages.order(last_message_at: :desc), per: 1
+    @conversations = paginated current_user.conversations.with_messages.order(last_message_at: :desc), per: 10
   end
 end
