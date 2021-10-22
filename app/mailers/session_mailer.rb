d87e47c92c917553909944299f0b796a69ba0499
Jens Ravens <jens@nerdgeschoss.de>
2021-10-22 12:36:44 +0200

    styled login mail

--- app/mailers/session_mailer.rb
+++ app/mailers/session_mailer.rb
@@ -3,6 +3,7 @@
 class SessionMailer < ApplicationMailer
   def login(user)
     @user = user
+    @url = login_url(locale: I18n.locale, token: @user.secure_id(expires_in: 1.hour))
     mail to: user.email
   end
 end
