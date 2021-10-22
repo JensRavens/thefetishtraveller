8138b6f7951693e2520217c85938b33098511066
Jens Ravens <jens@nerdgeschoss.de>
2021-10-22 16:12:52 +0200

    add analytics

---
+++ app/javascript/controllers/analytics_controller.ts
@@ -0,0 +1,26 @@
+import { Controller } from 'stimulus';
+
+interface DataEvent {
+  event: string;
+}
+
+export default class extends Controller {
+  dataLayer!: [DataEvent];
+
+  connect(): void {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (window as any).dataLayer = (window as any).dataLayer || [];
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    this.dataLayer = (window as any).dataLayer;
+    this.recordPage();
+  }
+
+  recordPage(): void {
+    const event = {
+      event: 'Pageview',
+      path: location.pathname + location.search,
+      host: location.host,
+    };
+    this.dataLayer.push(event);
+  }
+}
