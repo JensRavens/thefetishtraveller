6bade88a706b1d6ab386e5c256be8b1d9ef65f5a
Jens Ravens <jens@nerdgeschoss.de>
2021-10-29 17:06:41 +0200

    readd rails ujs

--- app/javascript/application.ts
+++ app/javascript/application.ts
@@ -1,5 +1,8 @@
+import * as Rails from '@rails/ujs';
 import '@hotwired/turbo-rails';
 import 'form-request-submit-polyfill';
+
+Rails.start();
 
 import './sprinkles/touch';
 import './controllers';
