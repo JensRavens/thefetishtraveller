124c9e1d63d7fabb0916f684c364dab9787eb232
Jens Ravens <jens@nerdgeschoss.de>
2021-11-07 13:11:39 +0100

    lint

--- app/validators/social_network_validator.rb
+++ app/validators/social_network_validator.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class SocialNetworkValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    return unless value.present?
+    return if value.blank?
 
     record.errors.add attribute, I18n.t("errors.messages.at_not_allowed"), **options if value.start_with? "@"
     record.errors.add attribute, I18n.t("errors.messages.no_url_allowed"), **options if value.include? "/"
