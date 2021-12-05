3903193aaeb7d767c56cb17ab72a53264c9e40d3
Jens Ravens <jens@nerdgeschoss.de>
2021-12-05 12:25:18 +0100

    json ld tags

--- app/helpers/meta_helper.rb
+++ app/helpers/meta_helper.rb
@@ -25,4 +25,50 @@ module MetaHelper
   def canonical(path)
     content_for :canonical_url, path, flush: true
   end
+
+  def article_json_ld(article)
+    {
+      "@context": "https://schema.org",
+      "@type": "NewsArticle",
+      headline: strip_tags(article.title),
+      image: [image_asset_url(article.hero, relative: false)].compact,
+      datePublished: article.publish_at,
+      dateModified: article.updated_at,
+      author: [{
+        "@type": "Person",
+        name: article.user.public_name,
+        url: profile_url(article.user)
+      }]
+    }
+  end
+
+  def event_json_ld(event)
+    {
+      "@context": "https://schema.org",
+      "@type": "Event",
+      name: event.name,
+      startDate: event.start_at,
+      endDate: event.end_at,
+      eventAttendanceMode: "https://schema.org/OfflineEventAttendanceMode",
+      eventStatus: "https://schema.org/EventScheduled",
+      location: {
+        "@type": "Place",
+        name: event.location.name,
+        address: {
+          "@type": "PostalAddress",
+          streetAddress: event.location.address,
+          addressLocality: event.location.city,
+          postalCode: event.location.zip,
+          addressCountry: event.location.country_code
+        }
+      },
+      image: [image_asset_url(event.hero, relative: false), image_asset_url(event.header, relative: false), image_asset_url(event.flyer, relative: false)].compact,
+      description: event.abstract,
+      organizer: {
+        "@type": "Organization",
+        name: event.organizer_name,
+        url: event.website
+      }
+    }
+  end
 end
