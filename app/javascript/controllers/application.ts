1573a5ca13cc5aade915a10e16dc0835ea90df63
Jens Ravens <jens@nerdgeschoss.de>
2022-02-20 12:33:45 +0100

    global app instance

--- app/javascript/controllers/application.ts
+++ app/javascript/controllers/application.ts
@@ -1,7 +1,14 @@
 import { Application } from '@hotwired/stimulus';
 
+declare global {
+  interface Window {
+    app: Application;
+  }
+}
+
 const application = Application.start();
 
 application.debug = false;
+window.app = application;
 
 export { application };
