c8856222997112dd5c94170944ca483e865c64e4
Jens Ravens <jens@nerdgeschoss.de>
2022-10-09 21:37:58 +0200

    add specs for subevents

--- spec/system/event_spec.rb
+++ spec/system/event_spec.rb
@@ -5,11 +5,13 @@ require "system_helper"
 RSpec.describe "user in the feed" do
   fixtures :all
   let(:user) { users(:freddy) }
+  let(:event) { events(:folsom) }
 
   before { login(user) }
 
   it "creates an event" do
     visit events_path
+    screenshot "events"
     click_on "submit_event"
 
     fill_in "name", with: "My new event"
@@ -20,5 +22,21 @@ RSpec.describe "user in the feed" do
     click_on "Create"
 
     expect(page).to have_content "My new event"
+  end
+
+  it "creates a subevent" do
+    event.owners << user
+    visit event_path(event)
+    click_on "add_event"
+
+    fill_in "name", with: "BLUF Social"
+    find("#event_location_details_id", visible: false).set("berlin")
+    check "official"
+    screenshot "create_subevent"
+    click_on "Create"
+
+    click_on "Folsom", match: :first
+    expect(page).to have_content "BLUF Social"
+    screenshot "event"
   end
 end
